f5f222ae4b392efb27ffe00c7cac11952d5537ca
Stephen Klippenstein <sjk@anl.gov>
2019-01-08 14:52:57 -0600

    fixes debug install script

--- debug/fake-install.sh
+++ debug/fake-install.sh
@@ -1,5 +1,5 @@
 # run with: . /path/to/fake-install.sh
-export THIS_DIR="`pwd`/`dirname "$BASH_SOURCE"`"
+export THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PROJECT_ROOT=$THIS_DIR/..
 
 export PATH=$THIS_DIR/bin:$PROJECT_ROOT/exe:$PATH
