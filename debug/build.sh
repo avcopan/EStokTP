640e69d7b8c3d034090476987eb0ac49330a78bf
avcopan <avcopan@gmail.com>
2019-01-08 15:27:26 -0600

    Same fix for debug build script

--- debug/build.sh
+++ debug/build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
+export THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export PROJECT_ROOT=$THIS_DIR/..
+
 export CC=$CONDA_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc
 export FC=$CONDA_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gfortran
 
 # debug flags:
 export CFLAGS="-march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-all -fno-plt -Og -g -Wall -Wextra -fvar-tracking-assignments -pipe -I${CONDA_PREFIX}/include"
 export FFLAGS="-fopenmp -march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-strong -O2 -pipe -I${CONDA_PREFIX}/include -Og -g -Wall -Wextra -fcheck=all -fbacktrace -fimplicit-none -fvar-tracking-assignments -pipe"
-
-export THIS_DIR="`pwd`/`dirname "$BASH_SOURCE"`"
-export PROJECT_ROOT="`pwd`/`dirname "$BASH_SOURCE"`"/..
 
 mkdir -p $THIS_DIR/build
 cd $THIS_DIR/build
